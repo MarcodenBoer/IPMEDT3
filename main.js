5ad297df8228b6b92d2b1097cd04abb5bd63aa7e
MarcodenBoer <marco.denboer2000@gmail.com>
2021-01-20 14:11:25 +0100

    new folder structure

--- main.js
+++
@@ -1,6 +0,0 @@
-const animals = document.getElementsByClassName('animals');
-
-for (let i = 0; i < animals.length; i++) {
-  animals[i].addEventListener('click', function(evt){animals[i].components.sound.playSound();
-  console.log(i);})
-}
