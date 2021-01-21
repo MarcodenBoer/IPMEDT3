9b4cac9d4084899a27009c1efebe898222ca1bd6
Kasper <s1118202@student.hsleiden.nl>
2021-01-21 10:59:25 +0100

    Foto functie is inplaats van een foto ia camera nu via screen capture en dus maakt die ook een foto van de dieren

--- foto.js
+++ foto.js
@@ -1,43 +1,47 @@
 window.onload = function (){
   const takePhoto = document.getElementById('js--camera');
-  let snapshot = document.getElementById( "snapshot" );
+  const snapshot = document.getElementById( "snapshot" );
   let capture = document.getElementById( "capture" );
-  let video = document.getElementById('arjs-video'); //Pakt de camera van aframe ar
   let download = document.getElementById('js--download');
-  let cancel = document.getElementById('js--cancel');
+  const cancel = document.getElementById('js--cancel');
   let image = document.getElementById('js--image');
-  
 
-  function captureSnapshot() {
-    video = document.getElementById('arjs-video'); //Soms is de let video niet goed ingeladen en heeft die de waarde null waardoor er een error op line 14 komt.
-    capture.setAttribute("width", video.style.width); //pakt de width van de camera
-    capture.setAttribute("height", video.style.height); //pakt de hoogte van de camera, hierdoor wordt de img niet uitgerekt
-    capture.style.display = "none";
+  capture.style.display = "none";
 
-    if (video != null) {
-      let ctx = capture.getContext('2d');
-      let img = new Image();
 
-      ctx.drawImage( video, 0, 0, capture.width, capture.height ); //Failed to execute 'drawImage' deze error komt soms voor doordat het element video nog niet ingeladen is en dus null terug geeft.
+  const takeScreenShot = async() => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: {mediaSource: "screen"}
+    });
 
-      img.setAttribute("src", capture.toDataURL("image/jpeg"));
-      img.setAttribute("width", capture.getAttribute("width"));
+    const track = stream.getVideoTracks()[0];
+    const frame = new ImageCapture(track);
+    const bitmap = await frame.grabFrame();
 
-      image.innerHTML = "";
-      image.appendChild(img);
+    track.stop();
 
-      download.setAttribute("href", img.getAttribute("src"));
+    capture.width = bitmap.width;
+    capture.height = bitmap.height;
 
-      img.style.display = "block";
-      img.style.width = "100%";
+    const context = capture.getContext('2d');
+    context.drawImage(bitmap, 0, 0, 790, bitmap.height/2);
+    const img = capture.toDataURL("image/jpeg");
 
-      displayPhoto();
-    }
-  }
+    download.setAttribute("href", img);
+    displayPhoto();
+
+  };
 
   function displayPhoto(){
+    let img = new Image();
+    img.setAttribute("src", capture.toDataURL("image/jpeg"));
+    image.innerHTML = "";
+    image.appendChild(img);
+
+    img.style.display = "block";
+    img.style.width = "100%";
+
     snapshot.style.display = "grid";
-    takePhoto.style.display = "none";
   }
 
   function photoDisplayed() {
@@ -45,7 +49,7 @@ window.onload = function (){
     takePhoto.style.display = "block";
   }
 
-  takePhoto.addEventListener("click", captureSnapshot);
+  takePhoto.addEventListener("click", takeScreenShot);
   download.addEventListener("click", photoDisplayed);
   cancel.addEventListener("click", photoDisplayed);
 
